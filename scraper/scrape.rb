f7c9b79d530c1e4b2cc6d76e201c3ee6dc9abcd1
Sean Brewer <seabre986@gmail.com>
2015-01-14 14:18:15 -0500

    Work on scraper. Add more scraped files.

--- scraper/scrape.rb
+++ scraper/scrape.rb
@@ -5,8 +5,15 @@ require 'uri'
 idx = "http://www.law.cornell.edu/supct/party.php?party=1&begin=19900101&end=20160101"
 
 def scrape(url)
-  n = Nokogiri::HTML(open(url)).at_css("casecontent")
-  n.to_s
+  n = Nokogiri::HTML(open(url))
+  n.encoding = 'utf-8'
+  casecontent = n.at_css("casecontent")
+  title = n.at_css("h1.title")
+  result = <<-eos
+#{title}
+#{casecontent}
+  eos
+  result
 end
 
 def get_docket(url)
@@ -17,7 +24,11 @@ end
 
 Nokogiri::HTML(open(idx)).css("ul li a").each do |c|
   if c[:href].include?("/supremecourt/text")
-    File.open("../html/#{get_docket(c[:href])}.html", 'w') {|f| f.write(scrape("http://www.law.cornell.edu" + c[:href])) }
+    begin
+      File.open("../html/#{get_docket(c[:href])}.html", "w:UTF-8") {|f| f.write(scrape("http://www.law.cornell.edu" + c[:href])) }
+    rescue
+      puts "Unable to do docket #{get_docket(c[:href])}!"
+    end
   else
     next
   end
